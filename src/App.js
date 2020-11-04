33440cc4fd70d6d51c731c7f87123d2848c07442
Joey Lopez <joeyl509625@gmail.com>
2020-11-03 21:04:13 -0600

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import './App.css';
+import { Route } from 'react-router-dom'
 import Home from './components/Home';
 import Navigation from './components/Navigation';
-import { Route } from 'react-router-dom'
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
