73ffb865ce22034355ede76e24c468a3f1816132
Joey Lopez <joeyl509625@gmail.com>
2020-11-06 19:28:22 -0600

    added

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,9 @@ function Home() {
 
     return (
         <Container className='hero text-center'>
-           <h1 className="hero-title">Hi. I'm <span>Joey</span></h1> 
+            <div className="homeInfo">
+           <h1 className="hero-title">Hi. I'm <span>Joey</span></h1>
+           </div> 
            <h4>A Web Developer</h4>     
             <p className="hero_sub lead">Full-Stack developer with a special nack for React and NodeJs.</p>
           <section className='hero-img'>
