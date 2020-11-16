fcb1e1fad3b253b4b29e5ad1575341e700f70658
Joey Lopez <joeyl509625@gmail.com>
2020-11-16 15:16:25 -0600

    added icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaRegEnvelope } from 'react-icons/fa'
+import { FaRegEnvelope, FaGithubAlt, FaLinkedinIn } from 'react-icons/fa'
 
 function Contact() {
     return (
@@ -7,14 +7,14 @@ function Contact() {
            <h1 className>Contact Me!</h1>
            <h3>I'm available for freelance work as well.</h3>
            <h3 className='lead'>Reach out to me so we can chat.</h3>
-           <p className='email-link'><a href="mailto:joeyl509625@gmail.com">Email Me: <span className='icon'><FaRegEnvelope /> </span></a></p>
+           <p className='email-link'><a href="inquiry@joeylopez.dev"><span className='icon'><FaRegEnvelope /> inquiry@joeylopez.dev </span></a></p>
         <div  className='contact-links'>
           <ul className='links'>
               <li>
-                 <a href="https://www.linkedin.com/in/joeylopez0587/"  rel='noopener noreferrer' target='_blank'>Linked in</a>
+                 <a href="https://www.linkedin.com/in/joeylopez0587/"  rel='noopener noreferrer' target='_blank'> <FaLinkedinIn color='red' size='2em' /> </a>
               </li>
               <li>
-                 <a href="https://github.com/BobbaGunoosh" rel='noopener noreferrer' target='_blank'>Github</a> 
+                 <a href="https://github.com/BobbaGunoosh" rel='noopener noreferrer' target='_blank'> <FaGithubAlt color='red' size='2em' /> </a> 
               </li>
           </ul>
         </div>
